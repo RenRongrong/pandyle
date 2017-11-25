a3090b11dcdddf8ebf35eec4535a1ce315d4c627
荣荣 任 <370828942@qq.com>
2017-11-25 13:47:19 +0800

    update20171124

---
+++ pandyle.js
@@ -0,0 +1,18 @@
+/// <reference path="node_modules/@types/jquery/index.d.ts" />
+
+$(document).ready(function() {
+    $('.flex').each(function(index, ele) {
+        if ($(ele).data('gap')) {
+            var gap = parseFloat($(ele).data('gap')) / 2 + 'px';
+            if ($(ele).hasClass('vertical')) {
+                $(ele).children().css({ 'margin-top': gap, 'margin-bottom': gap });
+                $(ele).children().first().css('margin-top', '0');
+                $(ele).children().last().css('margin-bottom', '0');
+            } else {
+                $(ele).children().css({ 'margin-left': gap, 'margin-right': gap });
+                $(ele).children().first().css('margin-left', '0');
+                $(ele).children().last().css('margin-right', '0');
+            }
+        }
+    });
+})
