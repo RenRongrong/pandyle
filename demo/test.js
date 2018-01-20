51aa79720a023e746e49a01bd3be3539c3cf3cf6
荣荣 任 <370828942@qq.com>
2018-01-21 01:11:39 +0800

    日常提交

--- demo/test.js
+++ demo/test.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    var arr = [
-        { property: "content.id", element: 1 },
-        { property: "content.name.str", element: 2 },
-        { property: "type", element: 3 }
-    ];
-    var a1 = arr.filter(function(value) {
-        value.property === 'content.id';
-    })
-    console.log(a1);
     vm = new Pandyle.VM($('#temp'), {
         content: {
             id: 2,
