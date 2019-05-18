0e04967f02a0e3cddd5913938acdee209bb420e1
荣荣 任 <370828942@qq.com>
2019-05-18 22:45:22 +0800

    添加DomData接口

---
+++ src/core/interfaces/IDomData.ts
@@ -0,0 +1,24 @@
+interface IDomData{
+    context: any,
+    binding: IBinding,
+    pattern: string,
+    children: JQuery<HTMLElement>,
+    parent: JQuery<HTMLElement>,
+    parentProperty: string,
+    alias: IAlias,
+    pIndex: number
+}
+
+interface IBinding{
+    [key:string] : {
+        pattern: string,
+        related: boolean
+    }
+}
+
+interface IAlias{
+    [key:string] : {
+        data: any,
+        property: string
+    }
+}
