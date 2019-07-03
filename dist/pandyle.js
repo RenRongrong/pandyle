9839a17eb815807bb2d958cb29f32d691b6f3143
宋萍 <pandarrr@aliyun.com>
2019-07-03 13:35:37 +0800

    消除ts编译时关于reduce、escape和unescape的报错

--- dist/pandyle.js
+++ dist/pandyle.js
@@ -1241,23 +1241,23 @@ if (!Array.prototype.map) {
     };
 }
 if (!Array.prototype.reduce) {
-    Array.prototype.reduce = function (callback, opt_initialValue) {
+    Array.prototype.reduce = function (callbackfn, initialValue) {
         'use strict';
         if (null === this || 'undefined' === typeof this) {
             throw new TypeError('Array.prototype.reduce called on null or undefined');
         }
-        if ('function' !== typeof callback) {
-            throw new TypeError(callback + ' is not a function');
+        if ('function' !== typeof callbackfn) {
+            throw new TypeError(callbackfn + ' is not a function');
         }
         var index, value, length = this.length >>> 0, isValueSet = false;
         if (1 < arguments.length) {
-            value = opt_initialValue;
+            value = initialValue;
             isValueSet = true;
         }
         for (index = 0; length > index; ++index) {
             if (this.hasOwnProperty(index)) {
                 if (isValueSet) {
-                    value = callback(value, this[index], index, this);
+                    value = callbackfn(value, this[index], index, this);
                 }
                 else {
                     value = this[index];
